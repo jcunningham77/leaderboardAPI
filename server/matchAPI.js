1d10fc30527df024126a3565a02e13c989a18ad2
Jeff Cunningham <jcunningham77@gmail.com>
2018-08-03 21:23:34 -0400

    post match

--- server/matchAPI.js
+++ server/matchAPI.js
@@ -4,7 +4,7 @@ var Match = require('../model/matchModel');
 module.exports = function upload(app) {
 
     app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({extended:true}));
+    // app.use(bodyParser.urlencoded({extended:true}));
 
 
     app.get('/api/matches',function(req,res){
@@ -21,14 +21,28 @@ module.exports = function upload(app) {
 
 
     });
-
     app.post('/api/match',function(req,res){
 
+        console.log(JSON.stringify(req.body.data));
+        console.log('here');
         var match = new Match({
-            player1Name: req.body.data.player1Name,
-            player2Name: req.body.data.player2Name,
-            player1Score: req.body.data.player1Score,
-            player2Score: req.body.data.player2Score,
+            player1Name: req.body.player1Name,
+            player2Name: req.body.player2Name,
+            player1Score: req.body.player1Score,
+            player2Score: req.body.player2Score,
+        });
+
+
+
+        match.save(function (err, bet, numAffected) {
+            if (err) {
+                console.log(err);
+                res.status('500').send(err);
+
+            } else if (bet) {
+                console.log('successfully persisted ' + numAffected + ' bet = ' + bet);
+                res.status('200').send(bet);
+            }
         });
 
 
